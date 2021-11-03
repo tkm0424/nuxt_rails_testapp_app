95ea83a471a8bd45f458cb9c5e5346861b520226
tkm0424 <fcp.t4ku@gmail.com>
2021-11-04 08:21:22 +0900

    Add users_controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+
 # gem 'minitest-reporters' setup
 require "minitest/reporters"
 Minitest::Reporters.use!
@@ -11,12 +12,11 @@ class ActiveSupport::TestCase
   parallelize_setup do |worker|
     load "#{Rails.root}/db/seeds.rb"
   end
-  
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  def active_user
+    User.find_by(activated: true)
+  end
 end
