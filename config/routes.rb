45b3e4e4c11f306a8801c61c55e25d03874b911b
tkm0424 <fcp.t4ku@gmail.com>
2021-12-08 22:04:29 +0900

    Add auto token controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # api test action
-      resources :users, only:[:index]
+
+      #auth_token
+      resources :auth_token, only:[:create]do
+        post :refresh, on: :collection
+        delete :destroy, on: :collection
+      end
     end
   end
 end
