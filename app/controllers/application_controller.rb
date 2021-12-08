a98a8d124f43e0d6952f4df7a257b86183418e8c
tkm0424 <fcp.t4ku@gmail.com>
2021-12-08 20:31:11 +0900

    Add authenticate_service module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  #Cookieを扱う
+  include ActionController::Cookies
+  #認可を扱う
+  include UserAuthenticateService
 end
---
+++ app/services/user_authenticate_service.rb
@@ -0,0 +1,34 @@
+module UserAuthenticateService
+
+  # 認証済みのユーザーが居ればtrue、存在しない場合は401を返す
+  def authenticate_user
+    current_user.present? || unauthorized_user
+  end
+
+  private
+
+    # リクエストヘッダートークンを取得する
+    def token_from_request_headers
+      request.headers["Authorization"]&.split&.last
+    end
+
+    # access_tokenから有効なユーザーを取得する
+    def fetch_user_from_access_token
+      User.from_access_token(token_from_request_headers)
+    rescue UserAuth.not_found_exception_class,
+           JWT::DecodeError, JWT::EncodeError
+      nil
+    end
+
+    # tokenのユーザーを返す
+    def current_user
+      return nil unless token_from_request_headers
+      @_current_user ||= fetch_user_from_access_token
+    end
+
+    # 認証エラー
+    def unauthorized_user
+      cookies.delete(UserAuth.session_key)
+      head(:unauthorized)
+    end
+end
